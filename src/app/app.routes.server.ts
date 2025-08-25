6b6ec70bd7526aff48020948ab1aac9ad5636e87
Yaya-Go <lmy.wilks@outlook.com>
2025-08-25 16:25:10 -0400

    firebase

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,14 @@
+import { inject } from '@angular/core';
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { ProjectService } from './services/project.service';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'project/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
